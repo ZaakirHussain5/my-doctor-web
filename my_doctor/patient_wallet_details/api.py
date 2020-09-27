f67b47e0d734a89b018e2e032ec502b343aa86a5
zaakir hussain <zaakir@gmail.com>
2020-09-27 16:39:30 +0530

    dff

--- my_doctor/patient_wallet_details/api.py
+++ my_doctor/patient_wallet_details/api.py
@@ -2,7 +2,6 @@ from .serializers import patient_wallet_detailsSerializer, WalletSerializer
 from .models import patient_wallet_details
 from rest_framework import viewsets, permissions
 from django.core.exceptions import ObjectDoesNotExist
-
 from transactions.models import transactions
 
 class patient_wallet_detailsViewSet(viewsets.ModelViewSet):
@@ -14,15 +13,6 @@ class patient_wallet_detailsViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return patient_wallet_details.objects.all()
 
-    # def perform_create(self,serializer):
-    #     new_balance = self.request.data["balance"]
-    #     try:
-    #         existing = patient_wallet_details.objects.get(patient=self.request.user.id)
-    #         new_balance += existing.balance 
-    #     except ObjectDoesNotExist:
-    #         pass
-    #     serializer.save(patient_id=self.request.user,balance=new_balance)
-
 class WalletDetailView(viewsets.ModelViewSet):
     permission_classes=[
         permissions.IsAuthenticated
@@ -32,15 +22,4 @@ class WalletDetailView(viewsets.ModelViewSet):
     def get_queryset(self):
         return self.request.user.Wallet.all()
 
-    # def get(self, request, format=None):
-    #     wallet = patient_wallet_details.objects.all().values_list('patient', flat=True)
-    #     wallet_id = ""
-    #     for user in wallet:
-    #         wallet_id = user
-    #     patient = transactions.objects.filter(user_id__username = wallet_id)
-    #     return Response({   
-    #         "wallet_details":{
-    #             "id":str(wallet_id),
-    #             # "balanace":wallet_balance
-    #         }
-    #     }) 
+
