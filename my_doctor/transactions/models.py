591c4e67fa689ba23467afbc50953eb8a634312d
zaakir hussain <zaakir@gmail.com>
2020-09-27 19:46:36 +0530

    Updated Transactiona Model

--- my_doctor/transactions/models.py
+++ my_doctor/transactions/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from patient_wallet_details.models import patient_wallet_details
 
 class transactions(models.Model):
-    ref_id = models.CharField(max_length=25, null=True)
+    ref_id = models.CharField(max_length=250, null=True)
     user_id = models.ForeignKey(User,related_name="transaction",on_delete=models.CASCADE, null=True, blank=True)
     credit  = models.DecimalField(max_digits=10,decimal_places=2, blank=True, null=True, default=0)
     debit = models.DecimalField(max_digits=10,decimal_places=2, blank=True, null=True, default=0)
