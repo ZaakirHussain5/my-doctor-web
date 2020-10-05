ad7b17c3eff0c63eae28e9e95f69cdd52a8713cc
zaakir hussain <zaakir@gmail.com>
2020-10-05 15:24:53 +0530

    Done bit Fixing

--- my_doctor/doctors/admin.py
+++ my_doctor/doctors/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import doctors_info
+from .models import doctors_info,DoctorTimings
 # Register your models here.
 
 admin.site.register(doctors_info)
+admin.site.register(DoctorTimings)
