fb8622028d0e722fcc0c25983be990f276022cb2
zaakir hussain <zaakir@gmail.com>
2020-10-26 14:03:43 +0530

    video Calling Feature Added

--- my_doctor/vedio_chat/api.py
+++ my_doctor/vedio_chat/api.py
@@ -1,15 +1,44 @@
-from rest_framework import permissions,viewsets, mixins
+from rest_framework import permissions,viewsets, mixins,generics
+from rest_framework.response import Response
 from .models import VedioChat
 from .serializers import VedioChatSerializer
+from doctors.models import doctors_info
+from opentok import OpenTok,MediaModes
+opentok = OpenTok("46964534", "76ca83ec02f7c0ef904f536a2d0bc251df25d8a4")
 
 class vedioChatOparetion(viewsets.ModelViewSet):
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
     serializer_class = VedioChatSerializer
 
     def get_queryset(self):
-        return VedioChat.objects.all()
+        queryset = VedioChat.objects.all()
+        user = self.request.query_params.get('user', None)
+        if user is not None:
+            queryset = self.request.user.call_for.all()
+        return queryset
     
     def perform_create(self,serializer):
-        return serializer.save()    
+        return serializer.save()
+
+class InitiateCallAPI(generics.GenericAPIView):
+  serializer_class = VedioChatSerializer
+
+  def post(self, request, *args, **kwargs):
+    doctor_id = self.request.query_params.get('doctor_id',None)
+    session = opentok.create_session(media_mode=MediaModes.routed)
+    session_id = session.session_id
+    token = session.generate_token()
+    doctor = doctors_info.objects.get(id=doctor_id)
+    serializer = self.get_serializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.save(Call_from=self.request.user,Call_for = doctor.user,Call_session=session_id)
+    return Response({
+      "session_id": session_id,
+      "token": token
+    })
+
+
+
+
--- my_doctor/vedio_chat/models.py
+++ my_doctor/vedio_chat/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class VedioChat(models.Model):
-    Call_from = models.ForeignKey(User, related_name="call_from", on_delete=models.CASCADE)
-    Call_for = models.ForeignKey(User, related_name="vall_for", on_delete=models.CASCADE)
-    Call_Link = models.CharField(max_length=500)
+    Call_from = models.ForeignKey(User, related_name="call_from", on_delete=models.CASCADE,null=True,blank=True)
+    Call_for = models.ForeignKey(User, related_name="call_for", on_delete=models.CASCADE,null=True,blank=True)
+    Call_session = models.CharField(max_length=500,null=True,blank=True)
     
