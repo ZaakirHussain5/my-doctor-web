c991baec32f8444fa9bece7d40eb419af8e552c3
zaakir hussain <zaakir@gmail.com>
2020-10-29 13:00:28 +0530

    white noise done

--- my_doctor/vedio_chat/models.py
+++ my_doctor/vedio_chat/models.py
@@ -4,7 +4,6 @@ from patients.models import patient_info
 
 # Create your models here.
 class VedioChat(models.Model):
-<<<<<<< HEAD
     Call_from = models.ForeignKey(User, related_name="call_from", on_delete=models.CASCADE,null=True,blank=True)
     Call_for = models.ForeignKey(User, related_name="call_for", on_delete=models.CASCADE,null=True,blank=True)
     Call_session = models.CharField(max_length=500,null=True,blank=True)
@@ -13,18 +12,4 @@ class VedioChat(models.Model):
     def patient_name(self):
         return patient_info.objects.get(user__id=self.Call_from.id).full_name
     
-
-    
-
-=======
-<<<<<<< HEAD
-    Call_from = models.ForeignKey(User, related_name="call_from", on_delete=models.CASCADE)
-    Call_for = models.ForeignKey(User, related_name="vall_for", on_delete=models.CASCADE)
-    Call_Link = models.CharField(max_length=500)
-=======
-    Call_from = models.ForeignKey(User, related_name="call_from", on_delete=models.CASCADE,null=True,blank=True)
-    Call_for = models.ForeignKey(User, related_name="call_for", on_delete=models.CASCADE,null=True,blank=True)
-    Call_session = models.CharField(max_length=500,null=True,blank=True)
->>>>>>> fb8622028d0e722fcc0c25983be990f276022cb2
->>>>>>> f46f170114a7cb77da023a77f5d8842d519bd0da
     
