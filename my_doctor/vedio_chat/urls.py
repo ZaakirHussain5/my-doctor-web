c7e511747efb5b7884b0a061fe6bf41546081294
zaakir hussain <zaakir@gmail.com>
2020-11-30 20:29:39 +0530

    Done Some Changes

--- my_doctor/vedio_chat/urls.py
+++ my_doctor/vedio_chat/urls.py
@@ -3,7 +3,6 @@ from rest_framework import routers
 from django.urls import path, include
 from . import views
 
-
 router = routers.DefaultRouter()
 router.register('vedioChatOparetion', vedioChatOparetion, 'vedioChatOparetion')
 
