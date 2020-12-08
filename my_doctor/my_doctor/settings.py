002b56895c545339d917435a46f9d97d76ec1913
zaakir hussain <zaakir@gmail.com>
2020-12-08 20:15:33 +0530

    Email and Message added

--- my_doctor/my_doctor/settings.py
+++ my_doctor/my_doctor/settings.py
@@ -138,12 +138,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtpout.secureserver.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'doctorplus321@gmail.com'
 EMAIL_HOST_PASSWORD = 'doctor+plus'
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
+EMAIL_USE_SSL = False
 
 REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
