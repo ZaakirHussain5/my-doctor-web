5ba381f204f51b1e45af20f86942d9b80cf2b777
zaakir hussain <zaakir@gmail.com>
2020-12-08 18:25:00 +0530

    promo code model created

--- my_doctor/my_doctor/settings.py
+++ my_doctor/my_doctor/settings.py
@@ -139,10 +139,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtpout.secureserver.net'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'support@doctor-plus.in'
-EMAIL_HOST_PASSWORD = 'supportDoctor+'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'doctorplus321@gmail.com'
+EMAIL_HOST_PASSWORD = 'doctor+plus'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
--- my_doctor/promo_codes/models.py
+++ my_doctor/promo_codes/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 class promo_code(models.Model):
     code = models.CharField(max_length=10,unique=True)
+    discount_percent = models.DecimalField(max_digits=10,decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
