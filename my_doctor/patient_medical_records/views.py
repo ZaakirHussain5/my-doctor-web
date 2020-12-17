c1ad0999dee12b0e8d8bf14fae84bfffde7cffb6
zaakir hussain <zaakir@gmail.com>
2020-12-17 16:38:32 +0530

    Prescription Done

--- my_doctor/patient_medical_records/views.py
+++ my_doctor/patient_medical_records/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import patient_medical_records
 
-# Create your views here.
+def get_pres_by_consultation(request):
+    cons_id = request.GET.get('cons_id')
+    response = {}
+    response['pres_avl'] = True
+    try:
+       prescription = patient_medical_records.objects.get(consultation_id=cons_id)
+       response['prescription_file'] = prescription.record_files.url
+    except patient_medical_records.DoesNotExist:
+       response['pres_avl'] = False
+
+    return JsonResponse(response)
