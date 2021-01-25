ebad1939c88f51db4bc7016417a9c05e458a6888
zaakir hussain <zaakir@gmail.com>
2021-01-25 11:43:23 +0530

    Added Lastmodified in the serializer

--- my_doctor/patient_medical_records/serializers.py
+++ my_doctor/patient_medical_records/serializers.py
@@ -6,7 +6,7 @@ class MedicalRecordSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = patient_medical_records
-        fields = ('id', 'patient_name', 'patient', 'record_type', 'description', 'record_files', 'is_prescription', 'doctor', 'consultation_id')
+        fields = ('id', 'patient_name', 'patient', 'record_type', 'description', 'record_files', 'is_prescription', 'doctor', 'consultation_id','Last_modied')
 
 class MedicalRecordListSerializer(serializers.ModelSerializer):
     doctor = doctors_infoSerializer()
