471c7df43c65106459578e4a0924ca288ad97950
zaakir hussain <zaakir@gmail.com>
2021-02-22 16:01:34 +0530

    call without recieved fix

--- my_doctor/vedio_chat/views.py
+++ my_doctor/vedio_chat/views.py
@@ -57,7 +57,7 @@ def patientVideoChat(request):
 
 
 def ratings(request):
-    if request.GET.get('consultation') == '':
+    if request.GET.get('consultation') == '' or request.GET.get('consultation') == '0':
         return redirect('/patients/dashboard')
     return render(request,'video_chat/ratings.html', {'consultation': request.GET.get('consultation')})
 
